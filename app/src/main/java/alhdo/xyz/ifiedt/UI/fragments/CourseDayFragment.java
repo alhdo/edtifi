deb9de17c833bd01af1fbe41b2b6823749611e4c
Castro Alhdo <castroalhdo@gmail.com>
2018-11-10 11:23:58 +0700

    Communication betwenn Edt and day fragment

--- app/src/main/java/alhdo/xyz/ifiedt/UI/fragments/CourseDayFragment.java
+++ app/src/main/java/alhdo/xyz/ifiedt/UI/fragments/CourseDayFragment.java
@@ -8,12 +8,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.events.calendar.views.EventsCalendar;
-
-import org.jetbrains.annotations.Nullable;
-
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 import alhdo.xyz.ifiedt.R;
 import butterknife.BindView;
@@ -21,10 +22,10 @@ import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link EdtFragment#newInstance} factory method to
+ * Use the {@link CourseDayFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class EdtFragment extends Fragment implements EventsCalendar.Callback {
+public class CourseDayFragment extends Fragment implements EdtFragment.OnDaySelectedListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -34,15 +35,9 @@ public class EdtFragment extends Fragment implements EventsCalendar.Callback {
     private String mParam1;
     private String mParam2;
 
-    @BindView(R.id.eventsCalendar)
-    EventsCalendar eventsCalendar;
-
-    public interface OnDaySelectedListener{
-        void onDaySelected(Calendar calendar);
-    }
-
-    OnDaySelectedListener onDaySelectedListener;
-    public EdtFragment() {
+    @BindView(R.id.dateFragment)
+    TextView dateFragment;
+    public CourseDayFragment() {
         // Required empty public constructor
     }
 
@@ -52,11 +47,11 @@ public class EdtFragment extends Fragment implements EventsCalendar.Callback {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment EdtFragment.
+     * @return A new instance of fragment CourseDayFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static EdtFragment newInstance(String param1, String param2) {
-        EdtFragment fragment = new EdtFragment();
+    public static CourseDayFragment newInstance(String param1, String param2) {
+        CourseDayFragment fragment = new CourseDayFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -71,42 +66,26 @@ public class EdtFragment extends Fragment implements EventsCalendar.Callback {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        EdtFragment fragment = (EdtFragment) getParentFragment();
+        assert fragment != null;
+        fragment.setOnDaySelectedListener(this);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view= inflater.inflate(R.layout.fragment_edt, container, false);
-        ButterKnife.bind(this, view);
-        Calendar today = Calendar.getInstance();
-        Calendar end = Calendar.getInstance();
-        eventsCalendar.setToday(today);
-        eventsCalendar.setSelectionMode(eventsCalendar.getSINGLE_SELECTION());
-        eventsCalendar.setMonthRange(today,end);
-        eventsCalendar.setCallback(this);
-        end.add(Calendar.YEAR,2);
+        View view= inflater.inflate(R.layout.fragment_course_day, container, false);
+        ButterKnife.bind(this,view);
+
         return view;
     }
 
-    public void setOnDaySelectedListener(OnDaySelectedListener onDaySelectedListener){
-        this.onDaySelectedListener=onDaySelectedListener;
-    }
-
     @Override
-    public void onDayLongPressed(@Nullable Calendar calendar) {
-
-    }
-
-    @Override
-    public void onDaySelected(@Nullable Calendar calendar) {
-            if(onDaySelectedListener!=null){
-                onDaySelectedListener.onDaySelected(calendar);
-            }
-    }
-
-    @Override
-    public void onMonthChanged(@Nullable Calendar calendar) {
-
+    public void onDaySelected(Calendar calendar) {
+        Date date = calendar.getTime();
+        Toast.makeText(getActivity(), new SimpleDateFormat("yyyy-MM-dd", Locale.FRENCH).format(date), Toast.LENGTH_SHORT).show();
+        dateFragment.setText(new SimpleDateFormat("yyyy-MM-dd", Locale.FRENCH).format(date));
     }
 }
